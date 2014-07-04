2374fe1a9300b45c8525809c10878360d23d80ba
Rafał Jagielski <rafal@uniontech.co.uk>
2014-07-04 12:16:19 +0100

    Displaying html body in admin detail view
    I'm also escaping quotes

--- email_log/admin.py
+++ email_log/admin.py
@@ -3,13 +3,22 @@ from django.contrib import admin
 from .models import Email
 
 
+BODY_FRAME = """\
+<iframe id="html_body" width='800' height='600'></iframe>
+<script>
+    if(!$){{$ = django.jQuery;}}
+    $('#html_body').get()[0].contentWindow.document.write('{}');
+</script>
+""".replace('\n', '')
+
 class EmailAdmin(admin.ModelAdmin):
     list_display = ['recipients', 'from_email', 'subject', 'date_sent', 'ok']
+    list_display_links = ['subject',]
     list_filter = ['date_sent', 'ok']
+    fields = ['recipients', 'from_email', 'subject', '_html_body', 'date_sent', 'ok']
     readonly_fields = ['from_email', 'recipients', 'subject', 'body',
                        '_html_body', 'date_sent', 'ok']
     search_fields = ['subject', 'body', 'recipients']
-    exclude = ['html_body']
 
     def has_delete_permission(self, *args, **kwargs):
         return False
@@ -18,7 +27,9 @@ class EmailAdmin(admin.ModelAdmin):
         return False
 
     def _html_body(self, obj):
-        return "<iframe width='800' height='600'></iframe><script>if(!$){$ = django.jQuery;}$('iframe').get()[0].contentWindow.document.write('" + obj.html_body + "');</script>"
+        escaped = obj.html_body.replace("'", r"\'")
+        return BODY_FRAME.format(escaped)
+
     _html_body.short_description = 'HTML body'
     _html_body.allow_tags = True
 
